256625b0d21fba4e073f266d523515478c9e4041
Ronn02 <72732177+Ronn02@users.noreply.github.com>
2023-07-17 22:26:47 +0800

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -20,11 +20,11 @@ const Login = () => {
       <div className="flex justify-center items-center flex-col h-[70vh] bg-[#FBE470]">
         <form className="flex flex-col items-center">
           <h2>Login</h2>
-          <input type="text" placeholder="Username" required className="inputField" />
-          <input type="password" placeholder="Password" required className="inputField" />
-          <button className='bg-[#6F87BE] hover:border-black border border-blue-600 text-white rounded-full py-2 px-4 w-56 my-4'>
+          <input type="text" placeholder="Username" required className="inputField m-[5px] p-[5px] " />
+          <input type="password" placeholder="Password" required className="inputField m-[5px] p-[5px]" />
+            <a href='home'><button className='bg-[#6F87BE] hover:border-black border border-blue-600 text-white rounded-full py-2 px-4 w-56 my-4'>
             Login
-          </button>
+          </button></a>
           <p>
             Don't have an account? <a href="createAccount">Create one</a>
           </p>
