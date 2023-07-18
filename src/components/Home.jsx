1e1893a4eee6ba2f31a8aaa26a58df2053c1acd3
Ronn02 <72732177+Ronn02@users.noreply.github.com>
2023-07-19 01:48:57 +0800

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,29 +8,38 @@ const Home = () => {
   return (
     <div name='home' className='w-full min-h-screen'>
       <div className='h-[100px] w-full top-0 flex justify-end'>
-        
-      <div className='mr-[50px] mt-[20px] flex justify-center items-center rounded-full overflow-hidden bg-blue-100 h-[70px] w-[70px]'>
+        <div className='mr-[50px] mt-[20px] flex justify-center items-center rounded-full overflow-hidden bg-blue-100 h-[70px] w-[70px]'>
           <p className=''>User Pic</p>
         </div>
       </div>
 
-      <div className='m-[30px] '>
-        <h1 className='text-6xl font-bold'>Tara Gala?</h1>
-       
-        <li className='list-none flex justify-start m-[30px]'>
-          <a href="/"><ul className='h-[100px] w-[100px] flex flex-col justify-center items-center m-[10px] p-[10px] border-black border-2 rounded-[20px]'>
-            <img src={ToDo} alt='' className='h-[50px] mb-[15px]' />
-            To-D1
-          </ul></a>
-          <a href="/"> <ul className='h-[100px] w-[100px] flex flex-col justify-center items-center m-[10px] p-[10px] border-black border-2 rounded-[20px]'>
-            <img src={Expenses} alt='' className='h-[50px] mb-[15px]' />
-            Expenses
-          </ul></a>
-          <a href="/"> <ul className='h-[100px] w-[100px] flex flex-col justify-center items-center m-[10px] p-[10px] border-black border-2 rounded-[20px]'>
-            <img src={Message} alt='' className='h-[50px] mb-[15px]' />
-            Social
-          </ul></a>
+      <div className='m-[30px] bg-red-100'>
+        <h1 className='text-5xl sm:text-6xl font-bold'>Tara Gala?</h1>
+
+        <li className='list-none flex justify-start m-[30px] '>
+          <a href="">
+            <ul className='h-[70px] w-[70px] sm:h-[100px] sm:w-[100px] flex flex-col justify-center items-center m-[2px] sm:m-[10px] p-[10px] border-black border-2 rounded-[20px] hover:bg-[#FBE470]'>
+              <img src={ToDo} alt='' className='h-[30px] sm:h-[50px] mb-[15px]' />
+              <p className='text-[10px] sm:text-[10px]'>To-D1</p>
+            </ul>
+          </a>
+          <a href="">
+            <ul className='h-[70px] w-[70px] sm:h-[100px] sm:w-[100px] flex flex-col justify-center items-center m-[2px] sm:m-[10px] p-[10px] border-black border-2 rounded-[20px] hover:bg-[#FBE470]'>
+              <img src={Expenses} alt='' className='h-[30px] sm:h-[50px] mb-[15px]' />
+              <p className='text-[10px] sm:text-[10px]'>Expenses</p>
+            </ul>
+          </a>
+          <a href="">
+            <ul className='h-[70px] w-[70px] sm:h-[100px] sm:w-[100px] flex flex-col justify-center items-center m-[2px] sm:m-[10px] p-[10px] border-black border-2 rounded-[20px] hover:bg-[#FBE470]'>
+              <img src={Message} alt='' className='h-[30px] sm:h-[50px] mb-[15px]' />
+              <p className='text-[10px] sm:text-[10px]'>Social</p>
+            </ul>
+          </a>
         </li>
+      </div>
+
+      <div className='m-[30px] bg-blue-100'>
+      <h1 className='text-2xl sm:text-3xl font-bold'>Discover</h1>
       </div>
 
       <Navbar />
