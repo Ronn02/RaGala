1c5880211aefaf41bc54a832623e55f7f1418e8c
Ronn02 <72732177+Ronn02@users.noreply.github.com>
2023-07-19 15:42:30 +0800

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,17 +3,22 @@ import Navbar from './Navbar';
 import ToDo from '../assets/otherAssets/toDoIcon.png';
 import Expenses from '../assets/otherAssets/expensesIcon.png';
 import Message from '../assets/otherAssets/messageIcon.png';
+import Cavite from '../assets/destination/CAVITE/AGUINALDO SHRINE/AGUINALDO SHRINE, CAVITE - 1.jpg';
+import Laguna from '../assets/destination/LAGUNA/ENCHANTED KINGDOM/ENCHANTED KINGDOM, LAGUNA - 1.jpg';
+import Batangas from '../assets/destination/BATANGAS/CALERUEGA CHURCH/CALERUEGA CHURCH, BATANGAS - 1.jpg';
+import Rizal from '../assets/destination/RIZAL/AVILON ZOO/AVILON ZOO, RIZAL - 1.jpg';
+import Quezon from '../assets/destination/QUEZON/CAGBALETE ISLAND/CAGBALETE ISLAND, QUEZON - 1.jpg';
 
 const Home = () => {
   return (
     <div name='home' className='w-full min-h-screen'>
       <div className='h-[100px] w-full top-0 flex justify-end'>
-        <div className='mr-[50px] mt-[20px] flex justify-center items-center rounded-full overflow-hidden bg-blue-100 h-[70px] w-[70px]'>
+        <div className='mr-[30px] mt-[20px] flex justify-center items-center rounded-full overflow-hidden bg-blue-100 h-[70px] w-[70px]'>
           <p className=''>User Pic</p>
         </div>
       </div>
 
-      <div className='m-[30px] bg-red-100'>
+      <div className='m-[30px] '>
         <h1 className='text-5xl sm:text-6xl font-bold'>Tara Gala?</h1>
 
         <li className='list-none flex justify-start m-[30px] '>
@@ -38,8 +43,55 @@ const Home = () => {
         </li>
       </div>
 
-      <div className='m-[30px] bg-blue-100'>
+      <div className='m-[30px] '>
       <h1 className='text-2xl sm:text-3xl font-bold'>Discover</h1>
+      <p>Start exploring now and plan your next memorable trip!</p>
+      </div>  
+      {/*Container */}
+      <div className=' grid grid-cols-1 sm:grid-cols-3 gap-3 text-center py-8 ml-[30px] mr-[30px]'>
+
+      <a href=""><div className='relative h-[250px] w-[380px] '>
+        <img src={Cavite} alt="" className="object-cover h-full w-full" />
+        <div className="absolute top-0 left-0 h-full w-full flex flex-col justify-center items-center text-white bg-black bg-opacity-50">
+          <h2 className="text-2xl sm:text-3xl font-bold">CAVITE</h2>
+      
+        </div>
+      </div></a>
+
+
+      <a href=""><div className='relative h-[250px] w-[380px] '>
+        <img src={Laguna} alt="" className="object-cover h-full w-full" />
+        <div className="absolute top-0 left-0 h-full w-full flex flex-col justify-center items-center text-white bg-black bg-opacity-50">
+          <h2 className="text-2xl sm:text-3xl font-bold">LAGUNA</h2>
+      
+        </div>
+      </div></a>
+
+
+      <a href=""><div className='relative h-[250px] w-[380px] '>
+        <img src={Batangas} alt="" className="object-cover h-full w-full" />
+        <div className="absolute top-0 left-0 h-full w-full flex flex-col justify-center items-center text-white bg-black bg-opacity-50">
+          <h2 className="text-2xl sm:text-3xl font-bold">BATANGAS</h2>
+      
+        </div>
+      </div></a>
+
+      <a href=""><div className='relative h-[250px] w-[380px] '>
+        <img src={Rizal} alt="" className="object-cover h-full w-full" />
+        <div className="absolute top-0 left-0 h-full w-full flex flex-col justify-center items-center text-white bg-black bg-opacity-50">
+          <h2 className="text-2xl sm:text-3xl font-bold">RIZAL</h2>
+      
+        </div>
+      </div></a>
+
+      <a href=""><div className='relative h-[250px] w-[380px] '>
+        <img src={Quezon} alt="" className="object-cover h-full w-full" />
+        <div className="absolute top-0 left-0 h-full w-full flex flex-col justify-center items-center text-white bg-black bg-opacity-50">
+          <h2 className="text-2xl sm:text-3xl font-bold">QUEZON</h2>
+      
+        </div>
+      </div></a>
+        
       </div>
 
       <Navbar />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import Plus from '../assets/otherAssets/plusIcon.png'
 const Navbar = () => {
   return (
     <div className='flex justify-center items-center flex-col fixed bottom-0 mb-[40px] left-0 w-full'>
-      <div className='flex justify-evenly items-center bg-[#6F87BE] h-[60px] w-[40vh] rounded-[20px]'>
+      <div className='flex justify-evenly items-center bg-[black] h-[60px] w-[40vh] rounded-[20px]'>
         <a href="home"><img src={Home} alt="" /></a>
         <a href=""><img src={Plus} alt="" /></a>
       </div>
